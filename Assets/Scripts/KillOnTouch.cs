7c05f1da8ac7e79cab22b9c4ef15709ce1a80ffd
AbbyNode <AbbyNode@users.noreply.github.com>
2017-08-16 03:42:29 -0400

    Added death

---
+++ Assets/Scripts/KillOnTouch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillOnTouch : MonoBehaviour {
+	private void OnCollisionEnter2D(Collision2D collision) {
+		CheckForKills(collision.gameObject);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collider) {
+		CheckForKills(collider.gameObject);
+	}
+
+	void CheckForKills(GameObject gameObject) {
+		if (gameObject.CompareTag("Player")) {
+			SceneManager.LoadScene("GameOverScene");
+		}
+	}
+}
